28b1feb8038ca38609bb7ed24c75958b4397ca7b
simionhxa <kn23-d.simionenko@nubip.edu.ua>
2024-10-22 10:29:45 +0300

    first commit

--- tasks/task1.js
+++ tasks/task1.js
@@ -1,3 +1,4 @@
+
 "Ваша функція повинна робити GET-запит до вказаного URL і отримати дані."
 "Поверніть дані користувачів у форматі масиву"
 "Дані мають включати такі поля, як id та name."
@@ -5,9 +6,16 @@
 "https://jsonplaceholder.typicode.com/users - адреса куди робити запит"
 
 function fetchUsers() {
-  // Ваш код
+    return fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => {
+            const users = data.map(user => ({
+                id: user.id,
+                name: user.name
+            }));
+            return users;
+        })
+        .catch(error => console.error('Error:', error));
 }
-
-console.log(fetchUsers())
 
 module.exports = fetchUsers;
